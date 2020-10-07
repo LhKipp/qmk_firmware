d69cceed5e69f1d4df3df9cfa87667f68b8211d4
Leonhard Kipp <Leonhard.Kipp@web.de>
2020-10-07 11:22:38 +0200

    add F keys. Add del, ins, OS_LALT, SFT_ENT

--- keyboards/keebio/iris/keymaps/LhKipp/keymap.c
+++ keyboards/keebio/iris/keymaps/LhKipp/keymap.c
@@ -25,9 +25,11 @@ extern keymap_config_t keymap_config;
 #define _SPEC2 4
 
 #define SFT_ESC LSFT_T(KC_ESC) //Shift on press, Escape on tap
+#define SFT_ENT LSFT_T(KC_ENT) //Shift on press, Escape on tap
 #define LCTRL_SPC LCTL_T(KC_SPC) //Ctrl on press, Space on tap
 
 #define OS_LGUI OSM(MOD_LGUI) //LGUI as one shot key
+#define OS_LALT OSM(MOD_LALT) //LGUI as one shot key
 #define OS_SPEC1 OSL(_SPEC1) //LGUI as one shot key
 #define OS_SPEC2 OSL(_SPEC2) //LGUI as one shot key
 
@@ -45,15 +47,15 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_NO,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_NO,
+     KC_INS,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Z,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     SFT_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SLSH, KC_ENT,
+     SFT_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SLSH, SFT_ENT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     OS_LGUI, KC_Y,    KC_X,    KC_C,    KC_V,    KC_B,    SC,               DVORAK,  KC_N,    KC_M,    GER_COM, GER_DOT, KC_MINS ,KC_QUOT,
+     OS_LGUI, KC_Y,    KC_X,    KC_C,    KC_V,    KC_B,    SC,               DVORAK,  KC_N,    KC_M,    GER_COM, GER_DOT, KC_MINS ,KC_LALT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LALT, LCTRL_SPC, KC_RALT,                   KC_RALT, OS_SPEC1, OS_SPEC2
+                                    KC_LALT, LCTRL_SPC, KC_RALT,                 KC_RALT, OS_SPEC1, OS_SPEC2
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
 
   ),
@@ -74,13 +76,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_DVORAK] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_NO,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_NO,
+     KC_INS,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB,  KC_SCLN, KC_COMM, KC_DOT,  KC_P,    KC_Y,                               KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     SFT_ESC, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                               KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_ENT,
+     SFT_ESC, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                               KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    SFT_ENT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     OS_LGUI, KC_QUOT, KC_Q,    KC_J,    KC_K,    KC_X,    KC_RALT,           QWERT,  KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_SLSH,
+     OS_LGUI, KC_QUOT, KC_Q,    KC_J,    KC_K,    KC_X,    KC_RALT,           QWERT,  KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    OS_LALT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     KC_LALT , LCTRL_SPC,KC_RALT,                  KC_RALT,OS_SPEC1,OS_SPEC2
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -88,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_SPEC1] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_NO,
+     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      _______, KC_ASTR, KC_DQUO, KC_LEFT, KC_RGHT, KC_GRV,                             KC_1,    KC_DOWN, KC_UP,   KC_QUOT, KC_PIPE, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -102,11 +104,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_SPEC2] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      _______, KC_PSCR, _______, KC_BRIU, KC_BRID, _______,                            _______, KC_VOLD, KC_VOLU, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_F13,  _______, KC_HOME, KC_END,  _______,                            _______, KC_PGDN, KC_PGUP, _______, KC_F14,  _______,
+     _______, KC_F11,  KC_F12,  KC_HOME, KC_END,  _______,                            _______, KC_PGDN, KC_PGUP, KC_F13,  KC_F14,  _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      _______, KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  _______,          _______, KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,  _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
